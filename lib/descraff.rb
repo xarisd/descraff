54890bc28164faea497500b87d7f9633fb564487
xarisd <xaris.dimitriou@gmail.com>
2012-06-29 15:31:56 +0300

    Added descraff_model method to Kernel module so that it is available everywhere if you have included 'descraff'. This way you can 'descraff ClassA do ... end' to add descraff-stuff into a class with the delusion that you are not opening it. This functionality could be used to group all descraff-stuff-descriptions for all your models in a single seperate file

--- lib/descraff.rb
+++ lib/descraff.rb
@@ -2,3 +2,15 @@
 
 require_relative "descraff/version"
 require_relative "descraff/model"
+
+module Kernel
+  
+  def descraff_model(klass_or_module, &block)
+    fail "#{klass_or_module} is not a Class or Module" if (klass_or_module.class != Class && klass_or_module.class != Module)
+    klass_or_module.class_eval do 
+      include Descraff::Model
+      descraff &block
+    end
+  end
+  
+end
---
+++ test/test_descraff.rb
@@ -0,0 +1,58 @@
+# encoding: utf-8
+
+require 'helper'
+require 'descraff'
+
+class TestDescraff < Test::Unit::TestCase
+  
+  
+  context "ClassA and ClassB" do
+    setup do
+      @classA = Class.new
+      @classB = Class.new @classA
+    end
+
+    should "not have descraff method" do
+      assert_nil(@classA.methods.index(:descraff))
+    end
+    
+    context "Use descraff_model to add descraff to ClassA" do
+      setup do
+        descraff_model @classA do
+          action :edit
+        end  
+      end
+      
+      should "use have descraff method and 1 action" do
+        refute_nil(@classA.methods.index(:descraff))
+        assert_equal(1, @classA.descraff.actions.size)
+      end
+      
+      should "be able to open a previously descraffed Class or a subclass of it and add more stuff to it" do
+
+        @classA.class_eval do
+          descraff do
+            action :show 
+          end
+        end
+        assert_equal(2, @classA.descraff.actions.size)
+
+        @classB.class_eval do
+          descraff do
+            action :delete 
+          end
+        end
+        assert_equal(1, @classB.descraff.actions.size)
+        
+        
+      end
+      
+      
+    end
+
+    
+    
+  end
+  
+
+end
